519a70779e7beec8f2c27c5d4244510b77856f13
BlackSpider65 <bshukla2236@gmail.com>
2025-06-04 09:55:30 +0530

    Updated python.py

--- python.py
+++ python.py
@@ -4,3 +4,7 @@ re = [even.append(i) if i%2==0 else odd.append(i) for i in range(1,10)]
 print(f"The even number is: {even}")
 print(f"The odd number is: {odd}")
 print()
+
+#now updaate the code
+
+print("Modify the file")
